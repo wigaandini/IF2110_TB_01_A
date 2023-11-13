8c4b6a9f946fa0a776e160467c32d45aca70b5ba
wigaandini <erdiantiwiga925@gmail.com>
2023-11-14 06:47:39 +0700

    benerin driver utas

--- driver/utas_driver.c
+++ driver/utas_driver.c
@@ -7,11 +7,16 @@
 #include "../adt/listdinkicauan.c"
 #include "../adt/liststatikuser.c"
 #include "../adt/charmachine.c"
+#include "../adt/datetime.c"
+#include "../adt/friendmatrix.c"
 #include "../program/utas.c"
-
+#include "../adt/time.c"
+#include "../adt/matrix.c"
+#include "../adt/listdin.c"
+#include "../adt/pcolor.c"
 
 int main(){
-    Word w, command;
+    Word w, command, kata;
     ListLinierUtas listUtas;
     ListUtas listUtasGlobal;
     ListKicauan listKicau;
@@ -19,29 +24,33 @@ int main(){
     UtasType u;
     int idUser, idKicau, idUtas, index;
 
-    CreateListUtasGLobal(&listUtasGlobal, CAPACITYMAXLISTKICAUAN);
+    CreateListUtasGlobal(&listUtasGlobal, CAPACITYMAXLISTKICAUAN);
 
     idUser = 1; // sementara, nantinya nyesuaiin sama user yg lagi login
+    idKicau = 1; // sementara, nanti disesuaiin pas user login
     do {      
         printf(">> ");
         STARTSENTENCE();
-        command = splitCommand(&w, currentWord, 1);
+        command = currentWord;
+        kata = splitCommand(&w, command, 1);
+        displayWord(kata);
+        printf("\n");
         
-        if(compareString(command,"UTAS")){
+        if(compareString(kata,"UTAS")){
             idKicau = WordToInt(splitCommand(&w, currentWord, 2));
             BIKIN_UTAS(idKicau, &listKicau, &listUtas, &listUtasGlobal, idUser, &u);
         }
-        else if(compareString(command,"SAMBUNG_UTAS")){
+        else if(compareString(kata,"SAMBUNG_UTAS")){
             idUtas = WordToInt(splitCommand(&w, currentWord, 2));
             index = WordToInt(splitCommand(&w, currentWord, 3));
             SAMBUNG_UTAS(idUtas, index, &listUtas, &listUtasGlobal, idUser, &u);
         }
-        else if(compareString(command,"HAPUS_UTAS")){
+        else if(compareString(kata,"HAPUS_UTAS")){
             idUtas = WordToInt(splitCommand(&w, currentWord, 2));
             index = WordToInt(splitCommand(&w, currentWord, 3));
             HAPUS_UTAS(idUtas, index, &listUtas, &listUtasGlobal, idUser, &u);
         }
-        else if(compareString(command,"CETAK_UTAS")){
+        else if(compareString(kata,"CETAK_UTAS")){
             idUtas = WordToInt(splitCommand(&w, currentWord, 2));
             CETAK_UTAS(listUser, listUtasGlobal, listUtas, idUser, idUtas);
         }
