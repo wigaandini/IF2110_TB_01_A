70c5d18a12c62ffa967ffea679215367b756a373
wigaandini <erdiantiwiga925@gmail.com>
2023-11-16 07:43:00 +0700

    add driver user, driver utas

---
+++ driver/user_driver.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../adt/header/boolean.h"
+#include "../adt/wordmachine.c"
+#include "../adt/listlinierutas.c"
+#include "../adt/listdinkicauan.c"
+#include "../adt/liststatikuser.c"
+#include "../adt/charmachine.c"
+#include "../adt/datetime.c"
+#include "../adt/friendmatrix.c"
+#include "../program/utas.c"
+#include "../adt/time.c"
+#include "../adt/matrix.c"
+#include "../adt/listdin.c"
+#include "../adt/pcolor.c"
+#include "../program/user.c"
+
+int main(){
+    ListStatikUser l;
+    Word nama;// Initialize login status
+    CreateListStatikUser(&l);
+    boolean login = isLoggedIn(l, nama);
+
+    do {
+        printf(">> ");
+        STARTSENTENCE();
+        displayWord(currentWord);
+        printf("\n");
+
+        if (compareString(currentWord, "DAFTAR")) {
+            DAFTAR(&l, login);
+            printListofUser(l);
+        } else if (compareString(currentWord, "KELUAR")) {
+            KELUAR(&login);  // Pass login by reference
+        } else if (compareString(currentWord, "MASUK")) {
+            // Prompt for username
+            printf("Masukkan nama: ");
+            STARTSENTENCE();
+            nama = currentWord;
+
+            MASUK(&l, &login);  // Pass login and username by reference
+        } else if (compareString(currentWord, "TUTUP_PROGRAM")) {
+            TUTUP_PROGRAM();
+        }
+    } while (!compareString(currentWord, "TUTUP_PROGRAM"));
+
+    return 0;
+}
--- driver/utas_driver.c
+++ driver/utas_driver.c
@@ -16,7 +16,7 @@
 #include "../adt/pcolor.c"
 #include "../program/user.c"
 
-int main(){
+int main() {
     Word w, command, kata;
     ListLinierUtas listUtas;
     ListUtas listUtasGlobal;
@@ -27,36 +27,63 @@ int main(){
 
     CreateListUtasGlobal(&listUtasGlobal, CAPACITYMAXLISTKICAUAN);
 
-    idUser = 1; // sementara, nantinya nyesuaiin sama user yg lagi login
-    idKicau = 1; // sementara, nanti disesuaiin pas user login
-    do {      
+    idUser = 1; // temporarily, adjust as needed
+    idKicau = 1; // temporarily, adjust as needed
+
+    do {
         printf(">> ");
         STARTSENTENCE();
         command = currentWord;
-        kata = splitCommand(&w, command, 1);
-        displayWord(kata);
-        printf("\n");
-        
-        if(compareString(kata,"UTAS")){
-            idKicau = WordToInt(splitCommand(&w, currentWord, 2));
+        printf("%d\n", countWords(command));
+
+        if (countWords(command) == 1) {
+            // Only one word in the command, no need to split
+            displayWord(command);
+            printf("\n");
+            // Assign the command to kata for further comparison
+            kata = command;
+        } else {
+            // More than one word, split the command
+            kata = splitCommand(&w, command, 1);
+            displayWord(kata);
+            printf("\n");
+        }
+
+        if (compareString(kata, "UTAS")) {
+            idKicau = WordToInt(splitCommand(&w, command, 2));
+            printf("kata: ");
+            displayWord(kata);
+            printf("\n");
+            printf("idKicau: %d\n", idKicau);
             BIKIN_UTAS(idKicau, &listKicau, &listUtas, &listUtasGlobal, idUser, &u);
-        }
-        else if(compareString(kata,"SAMBUNG_UTAS")){
-            idUtas = WordToInt(splitCommand(&w, currentWord, 2));
-            index = WordToInt(splitCommand(&w, currentWord, 3));
+        } else if (compareString(kata, "SAMBUNG_UTAS")) {
+            idUtas = WordToInt(splitCommand(&w, command, 2));
+            index = WordToInt(splitCommand(&w, command, 3));
+            printf("kata: ");
+            displayWord(kata);
+            printf("\n");
+            printf("idUtas: %d\n", idUtas);
+            printf("index: %d\n", index);
             SAMBUNG_UTAS(idUtas, index, &listUtas, &listUtasGlobal, idUser, &u);
-        }
-        else if(compareString(kata,"HAPUS_UTAS")){
-            idUtas = WordToInt(splitCommand(&w, currentWord, 2));
-            index = WordToInt(splitCommand(&w, currentWord, 3));
+        } else if (compareString(kata, "HAPUS_UTAS")) {
+            idUtas = WordToInt(splitCommand(&w, command, 2));
+            index = WordToInt(splitCommand(&w, command, 3));
+            printf("kata: ");
+            displayWord(kata);
+            printf("\n");
+            printf("idUtas: %d\n", idUtas);
+            printf("index: %d\n", index);
             HAPUS_UTAS(idUtas, index, &listUtas, &listUtasGlobal, idUser, &u);
-        }
-        else if(compareString(kata,"CETAK_UTAS")){
-            idUtas = WordToInt(splitCommand(&w, currentWord, 2));
+        } else if (compareString(kata, "CETAK_UTAS")) {
+            idUtas = WordToInt(splitCommand(&w, command, 2));
+            printf("kata: ");
+            displayWord(kata);
+            printf("\n");
+            printf("idUtas: %d\n", idUtas);
             CETAK_UTAS(listUser, listUtasGlobal, listUtas, idUser, idUtas);
-        }
-        else if(compareString(kata, "TUTUP_PROGRAM")){
-            TUTUP_PROGRAM();
+        } else if (compareString(kata, "TUTUP_PROGRAM")) {
+            printf("Anda telah keluar dari program Burbir. Sampai jumpa di pertemuan berikutnya!\n");
+            exit(0);
         }
     } while (!compareString(kata, "TUTUP_PROGRAM"));
 }
