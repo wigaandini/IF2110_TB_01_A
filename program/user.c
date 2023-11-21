4409de97128dca12cc7a6546d7b839b3ef7a4b5b
Yusuf Ardian Sandi <113454186+Yusufarsan@users.noreply.github.com>
2023-11-22 05:07:07 +0700

    Perbaikin user.c (#17)
    * Ubah hasil printListofUser
    
    * Pindahin isi folder adtkicauan
    
    * Ubah hasil printListofUser
    
    * Ubah noHP dari char ke ListDin
    
    * Add pcolor
    
    * Add driver folder
    
    * Edit listdin.c dan .h menyesuaikan untuk noHP
    
    * Add displayMatrixFoto
    
    * Fix noHP
    
    * Add profil.c program
    
    * Change UserNoHPLength to UserNoHPNeff
    
    * Add profil.c done
    
    * Add profil_driver.c in driver
    
    * Edit profil.c
    
    * Edit profil.c lagi
    
    * Edit liststatikuser.c & .h
    
    * Fix ganti_profil->noHP
    
    * Completed user.c
    
    * Fix include in pcolor_driver.c
    
    * Fix compareString function
    
    * feat: add user and user_driver finally
    
    * fix: fix bug in user.c
    
    * fix: fix bug tipe akun in liststatikuser.c
    
    * fix: fix bug printListofUser
    
    * feat: Add profil.c and profil_driver.c
    
    * fix: fix conflict
    
    * fix: ya pokonya nge benerin lihat_profil
    
    * fix: nyesuain profil.c dengan main.c
    
    * fix: nge cover testcase yang belom di cover
    
    * fix: menyesuaikan getIfOfName dgn main
    
    * fix: sesuaikan profil.c dgn main
    
    * feat: Add profil and user feature to main
    
    * fix: perbaiki pesan MASUK
    
    * fix: perbaiki ubah foto profil
    
    * Perbaiki masalah print tuan bri

--- program/user.c
+++ program/user.c
@@ -85,7 +85,9 @@ void MASUK(ListStatikUser *l, boolean *isLoggedIn, int *id_login){      // belum
         *isLoggedIn = true;
         *id_login = getIdOfName(*l, nama);
 
-        printf("\nAnda telah berhasil masuk dengan nama pengguna Tuan Bri. Mari menjelajahi BurBir bersama Ande-Ande Lumut!\n\n");
+        printf("\nAnda telah berhasil masuk dengan nama pengguna ");
+        displayWord(nama);
+        printf(". Mari menjelajahi BurBir bersama Ande-Ande Lumut!\n\n");
     }
 }
 
